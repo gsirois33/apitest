66f4ae63325932221d182e95c4999ead1878016f
Regina Sirois <43552607+gsirois33@users.noreply.github.com>
2021-01-05 19:57:38 -0700

    Update most_basic_server.py

--- most_basic_server.py
+++ most_basic_server.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
+import time
 import random
 from flask import Flask, request
-import time
-#from flask_httpauth import HTTPBasicAuth
-from flask_restful import Resource, Api, abort
 
+from flask_restful import (
+    Resource, 
+    Api, 
+    abort
+)
 app = Flask(__name__)
 api = Api(app)
 
-#auth = HTTPBasicAuth()
 
 class Agent(Resource):
+    
     def get(self):
-        r = random.randint(1,100)
+        r = random.randint(1, 100)
+        
         if r < 80:
-            return {"Message":"Good checkin"},200
+            return {"Message":"Good checkin"}, 200
         elif r < 90:
-            return {"Message":"Bad request"},400
+            return {"Message":"Bad request"}, 400
+        
         time.sleep(random.random())
-        return {"Message":"Server error"},500
+        return {"Message":"Server error"}, 500
 
 api.add_resource(Agent, '/v1/checkin')
 
